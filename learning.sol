b9b7925f13b182cbc8047d3907271bdc402d19b1
chiagoziem <52287428+chiagoziem-code@users.noreply.github.com>
2022-03-25 16:47:58 +0100

    Add files via upload

---
+++ learning.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract Learning {
+    
+    uint public count = 1;
+
+       function incrementCount() public  {
+
+        count ++;
+    }
+
+}
